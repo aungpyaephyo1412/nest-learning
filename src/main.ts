50dfdee5a70b74d6fdc1b092e3d342db020f3aee
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-06-26 12:03:18 +0630

    feat: vercel

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors({
+  //   origin: process.env.FRONTEND_URL, // Add your frontend URL here
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  //   allowedHeaders: [
+  //     'Origin',
+  //     'X-Requested-With',
+  //     'Content-Type',
+  //     'Accept',
+  //     'Authorization',
+  //   ],
+  // });
   const config = new DocumentBuilder()
     .setTitle('Nest Test')
     .setDescription('')
